435e8298622bdf65f186e9d4f32f0dbb1159e169
SesiSenai1DE <sesisenai.1de@gmail.com>
2023-12-09 14:26:48 -0300

    Finalização do projeto
    Finalização do projeto "To do List"

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Todo from './Components/Todo';
 
 import "./App.css";
 import TodoForm from './components/TodoForm';
+import Search from './components/Search';
+import Filter from './components/Filter';
 
 
 function App() {
@@ -24,6 +26,11 @@ function App() {
 
 
   ]);
+
+  const [search, setSearch] = useState("");
+
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
 
   const addTodo = (text, category) => {
     const newTodos = [...todos, 
@@ -47,8 +54,26 @@ function App() {
   return (
   <div className='app'>
     <h1>Lista de Tarefas</h1>
+    <Search search={search} setSearch={setSearch} />
+    <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
     <div className='todo-list'>
-      {todos.map((todo)=>(
+      {todos
+        .filter((todo) =>
+        filter === "All"
+          ? true
+          : filter === "Completed"
+          ? todo.isCompleted
+          : !todo.isCompleted
+      )
+      .filter((todo) =>
+        todo.text.toLowerCase().includes(search.toLowerCase())
+      )
+      .sort((a, b) =>
+      sort === "Asc"
+        ? a.text.localeCompare(b.text)
+        : b.text.localeCompare(a.text)
+      )
+      .map((todo)=>(
         <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
       ))}
     </div>
